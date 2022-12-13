863b9288048d33a93fb0efa239b3e03e12c11265
unknown <ivancruzhn@gmail.com>
2022-12-13 09:26:34 -0600

    API URL enviroment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  apiURL: ' https://6ra76c8p2d.execute-api.us-west-2.amazonaws.com/Api',
   webSocketURL: 'wss://r5h1tmeqt7.execute-api.us-west-2.amazonaws.com/testing'
 };
